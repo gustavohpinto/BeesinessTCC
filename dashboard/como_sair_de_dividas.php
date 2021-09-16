f001fcc1538e6dab64c86c590f3d5374d8b7d262
Gustavo Pinto <64512499+gustavohpinto@users.noreply.github.com>
2021-09-16 14:59:17 -0300

    Beesiness 2.0

---
+++ dashboard/como_sair_de_dividas.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Como Sair de Dividas</title>
+    <!-- Favicon-->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/navbar.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php include 'navbar_lateral.php'?>
+    <!-- NAvbar custom-->
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#524666;">
+        <div class="container">
+            <a class="navbar-brand" href="#!">Documentos - Como Sair de Dividas</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        </div>
+    </nav>
+    <!-- Conteudo da Página-->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm">
+                <!-- Documento-->
+                <article>
+                    <!-- Cabeçalho-->
+                    <header class="mb-4">
+                        <!-- Titulo -->
+                        <h1 class="fw-bolder mb-1">Como Sair de Dividas</h1>
+                        <!-- Tags -->
+                        <a class="badge bg-secondary text-decoration-none link-light" href="#!">Finanças</a>
+                        <a class="badge bg-secondary text-decoration-none link-light" href="#!">Educação Financeira</a>
+                    </header>
+                    <!-- Imagem -->
+                    <figure class="mb-4"><img class="img-fluid rounded"
+                            src="./assets/sai_de_dividas.png" alt="..." /></figure>
+                    <!-- Conteudo do Post-->
+                    <section class="mb-5">
+                        <h2 class="fw-bolder mb-4 mt-5">Introdução</h2>
+                        <p class="fs-5 mb-4">
+                            Muitas pessoas ao decorrer da vida adulta tem problemas, que levam a dividas, agora com esse
+                            conteúdo aprenda algumas dicas de como sair delas ou evita-las.</p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">1º Passo</h2>
+                            <p class="fs-5 mb-4">
+                            Primeiro, anote os seus gastos. Ter uma noção do quanto esta gastando e onde mais está
+                            gastando ajuda a priorizar as contas, diminuir gastos, saber quanto está devendo, e
+                            principalmente saber os gastos inúteis.</p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">2º Passo</h2>
+                            <p class="fs-5 mb-4">
+                            Segundo, organize os seus orçamentos. Saber quanto você tem e o quanto pode gastar ajuda ao
+                            decorrer do mês e a gerir o que e quanto comprar.</p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">3º Passo</h2>
+                            <p class="fs-5 mb-4">
+                            Terceiro, converse com a família, ou com quem você mora, é crucial todos estarem a par da
+                            condição financeira que tem, ou estão.</p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">4º Passo</h2>
+                            <p class="fs-5 mb-4">
+                            Quarto, corte os gastos desnecessários, Estando em dividas ou evitando elas é importante por
+                            limites nos consumos. Utilize critérios simples, como não deixar a luz acessa quando sair de
+                            um cômodo.</p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">5º Passo</h2>
+                            <p class="fs-5 mb-4">
+                            Quinto, crie metas. Quando queremos algo, é importante se preparar para conseguir isso, aos
+                            poucos separe um dinheiro que não vai usar no mês e guarde para o que quer comprar, ou
+                            precisa.
+                        </p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">6º Passo</h2>
+                        <p class="fs-5 mb-4">
+                            Sexto, tenha um fundo de emergencia, nunca se sabe quando vai precisar ou ter problemas,
+                            muitas das pessoas conseguem dividas devido a problemas como quando o carro quebra,
+                            problemas de saúde e etc. Estar preparado para essas situações ajuda muito.
+
+                        </p>
+                        
+
+                            <h2 class="fw-bolder mb-4 mt-5">7º Passo</h2>
+                        <p class="fs-5 mb-4">
+                            Sétimo, auto avalie-se, pense no que vez você ficar com essas dividas, isso é necessário
+                            para te ajudar a prevenir em entrar em outra dividas por causa de circunstâncias parecidas.
+                        </p>
+
+                        <div class="alert alert-secondary">
+                            <h5>Fontes</h5>
+                            <br>
+                            <p>ARAUJO, F. <b>10 passos para sair das dívidas.</b> Disponível em: https:
+                                //www.serasa.com.br/ensina/seu-nome-limpo/10-passos-sair-das-dividas/?gclid=CjwKCAjw7fuJBhBdEiwA2lLMYUFWn9ALjEeZkErp7Acmz-AehOFe5UJXmUETS2-o6okpBOC5GYv9MhoCvggQAvD_BwE
+                                . Acesso em: 14 set. 2021.
+                            </p>
+                        </div>
+
+
+
+                    </section>
+                </article>
+            </div>
+        </div>
+    </div>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Beesines - Documentos&copy; 2021</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
